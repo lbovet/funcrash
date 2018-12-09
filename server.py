575bf103ab4982a3aa1077caf9e8be907fdc6644
Laurent Bovet <laurent.bovet@windmaster.ch>
2018-12-09 23:12:25 +0100

    Added server-side blacklisting

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import time
 
 class Server(object):
     scores = list()
+    blacklist = list()
 
     def __init__(self):
         try:
@@ -12,9 +13,19 @@ class Server(object):
         except ImportError:
             self.client = Client("localhost", "", "")
         self.client.subscribe("funcrash/local/score", self.score_recu)
+        self.client.subscribe("funcrash/global/censor", self.censor)
         self.state = State()
         if self.state["scores"]:
             self.scores = self.state.scores
+        if self.state["blacklist"]:
+            self.blacklist = self.state.blacklist
+
+    def censor(self, s):
+        self.blacklist.append(s.nom)
+
+    def add(self, l, s):
+        if s.nom not in self.blacklist:
+            l.append(s)
 
     def score_recu(self, recu):
         ajoute = False
@@ -26,18 +37,18 @@ class Server(object):
                 if s.score > recu.score or ajoute:
                     if s.nom == recu.nom:
                         if not ajoute:
-                            new_list.append(s)
+                            self.add(new_list, s)
                         ajoute = True
                     else:
-                        new_list.append(s)
+                        self.add(new_list, s)
                 else:
-                    new_list.append(recu)
+                    self.add(new_list, recu)
                     if s.nom != recu.nom:
-                        new_list.append(s)
+                        self.add(new_list, s)
                     ajoute = True
             self.scores = new_list
             if len(self.scores) < 10 and not ajoute:
-                self.scores.append(recu)
+                self.add(self.scores, recu)
         self.scores = self.scores[:10]
         self.client.publish("funcrash/global/scores", self.scores, True)
         self.state.scores = self.scores
